9ada67542cd733f578c161c88fd6a5cb6800080b
Alfredo Cosco <alfredo.cosco@gmail.com>
2015-12-28 00:38:02 +0100

    CI3,BP5,BS3,FA,DT,PORT,GAL

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-Codeigniter 3
-Boilerplate 5
-Bootstrap 3
+		<p>Get the fuel with <a href="https://www.codeigniter.com/">Codeigniter</a> MVC framework.</p>
+		<p>Shake with <a href="https://html5boilerplate.com/">HTML5Boilerplate</a> best practices.</p>
+		<p>Serve gracefully with <a href="http://getbootstrap.com/">Bootstrap</a> and more.</p>
+		
+		<p><span class="lead">Cocktail</span> is a fast front-end builder. It takes the basic code by <a href="http://ariok.github.io/codeigniter-boilerplate/">Codeigniter-Boilerplate</a> but upgrades Codeigniter to version 3 and HTML5Boileplate to the v. 5. Then it adds Bootstrap (v.3) and some useful scripts:</p>
+		<ul>
+			<li><a href="https://jquery.com/">jQuery 1.11.2</a>,</li>
+			<li><a href="https://fortawesome.github.io/Font-Awesome/">Font Awesome 4.0.5</a> <i class="fa fa-fonticons fa-lg"></i>,</li>
+			<li><a href="https://datatables.net/">DataTables</a>,</li> 
+			<li><a href="http://ashleydw.github.io/lightbox/">Bootstrap Lightbox</a>,</li> 
+		</ul>
+		<p>And some 'ad hoc' helpers and functions for page rendering:</p>
+		<ul>
+			<li>Portfolio</li>
+		</ul>	
+		
+		<p>Other libraries to include: Bootstrap Validator, Gallery, Equal size, Modal Boxjs. </p>
+		<p>Ad hoc helpers to write: Page presentation: dynamic menu, tabs, pills, accordion, </p>
 
-
-[1]: http://codeigniter.com/
-[2]: http://html5boilerplate.com/
--- application/views/pages/homepage.php
+++ application/views/pages/homepage.php
@@ -26,23 +26,24 @@
       <div><p><a class="btn btn-default" href="https://github.com/orazionelson/cocktail/archive/master.zip" role="button">Download Cocktail</a> <a class="btn btn-default" href="https://github.com/orazionelson/cocktail" role="button">View on github</a></p></div>
 	</div>
 </div>
-<div id="about" class="container"><article>
-	<h2>About</h2>
-	<p><span class="lead">Cocktail</span> is a fast front-end builder. It takes the basic code by <a href="http://ariok.github.io/codeigniter-boilerplate/">Codeigniter-Boilerplate</a> but upgrades Codeigniter to version 3 and HTML5Boileplate to the v. 5. Then it adds Bootstrap (v.3) and some useful scripts:</p>
-	<ul>
-		<li><a href="https://jquery.com/">jQuery 1.11.2</a>,</li>
-		<li><a href="https://fortawesome.github.io/Font-Awesome/">Font Awesome 4.0.5</a> <i class="fa fa-fonticons fa-lg"></i>,</li>
-		<li><a href="https://datatables.net/">DataTables</a>,</li> 
-		<li><a href="http://ashleydw.github.io/lightbox/">Bootstrap Lightbox</a>,</li> 
-	</ul>
-	<p>And some 'ad hoc' helpers and functions for page rendering:</p>
-	<ul>
-		<li>Portfolio</li>
-	</ul>	
-	
-	<p>Other libraries to include: Bootstrap Validator, Gallery, Equal size, Modal Boxjs. </p>
-	<p>Ad hoc helpers to write: Page presentation: dynamic menu, tabs, pills, accordion, </p>
-</article>
+<div id="about" class="container">
+	<article>
+		<h2>About</h2>
+		<p><span class="lead">Cocktail</span> is a fast front-end builder. It takes the basic code by <a href="http://ariok.github.io/codeigniter-boilerplate/">Codeigniter-Boilerplate</a> but upgrades Codeigniter to version 3 and HTML5Boileplate to the v. 5. Then it adds Bootstrap (v.3) and some useful scripts:</p>
+		<ul>
+			<li><a href="https://jquery.com/">jQuery 1.11.2</a>,</li>
+			<li><a href="https://fortawesome.github.io/Font-Awesome/">Font Awesome 4.0.5</a> <i class="fa fa-fonticons fa-lg"></i>,</li>
+			<li><a href="https://datatables.net/">DataTables</a>,</li> 
+			<li><a href="http://ashleydw.github.io/lightbox/">Bootstrap Lightbox</a>,</li> 
+		</ul>
+		<p>And some 'ad hoc' helpers and functions for page rendering:</p>
+		<ul>
+			<li>Portfolio</li>
+		</ul>	
+		
+		<p>Other libraries to include: Bootstrap Validator, Gallery, Equal size, Modal Boxjs. </p>
+		<p>Ad hoc helpers to write: Page presentation: dynamic menu, tabs, pills, accordion, </p>
+	</article>
 
 </div>
 <div id="contacts" class="container"><article>
